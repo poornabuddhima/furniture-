496a7aa0d7028e51c4f5ab2618487f89b85952bf
poornabuddhima <poornabuddhima8@gmail.com>
2026-06-07 10:29:18 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  float productcost=0;
+  float transportcost=0;
+  float packingcost=0;
+  float servicecost=0;
+  float total;
+  float bill;
+   printf("Enter Product Cost;");
+   scanf("%f",&productcost);
+   printf("Enter Transport Cost:");
+   scanf("%f",&transportcost);\
+   printf("Enter Packing Cost");
+   scanf("%f",&packingcost);
+   printf("Enter Service Cost");
+   scanf("%f",&servicecost);
+    return 0;
+}
